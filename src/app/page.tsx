97645566111bcd51b48f9f8e52d0ef0f4fed534b
Simon Enrique Ramirez Ferrer <Asper.Tk93@gmail.com>
2026-05-23 14:46:45 +0000

    quita lo de manuales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
           </span>
         </div>
         <div className="flex items-center gap-6">
-          <Link href="#" className="text-sm font-bold text-muted-foreground hover:text-primary transition-colors">Manuales</Link>
           <Link href="#" className="text-sm font-bold text-muted-foreground hover:text-primary transition-colors">Soporte</Link>
         </div>
       </header>
